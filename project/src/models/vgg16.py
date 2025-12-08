6922f2b02274ed77bc7cf82dad9c963968dcca49
Harry Gui <harrygui@mac.mynetworksettings.com>
2025-12-07 20:06:49 -0500

    vgg model

---
+++ project/src/models/vgg16.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class VGG16CIFAKE(nn.Module):
+    def __init__(self, pretrained=True, freeze_backbone=False):
+        super().__init__()
+
+        # Load VGG16 pretrained model
+        if pretrained:
+            self.backbone = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
+        else:
+            self.backbone = models.vgg16(weights=None)
+
+        # FEATURES = convolutional layers (backbone)
+        # CLASSIFIER = original FC layers 
+        in_features = self.backbone.classifier[6].in_features
+
+        # Replace classifier head with 2-class output
+        self.backbone.classifier[6] = nn.Linear(in_features, 2)
+
+        # Optionally freeze all convolutional layers
+        if freeze_backbone:
+            for param in self.backbone.features.parameters():
+                param.requires_grad = False
+
+    def forward(self, x):
+        return self.backbone(x)  # logits [B,2]
